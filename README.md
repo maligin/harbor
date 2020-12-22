9dd704f0f9e6ea11ab710ca2c8cd2ff510d57870
Denis Maligin <denis@maligin.de>
2020-12-22 23:39:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Introduction
+
+This is a Harbor docker compose stack built for docker swarm and is based on the Bitnami Harbor docker-compose, which can be found here:
+
+- https://github.com/bitnami/bitnami-docker-harbor-registry
+
+The stack is using nfs volumes as persistent storage as well as Traefik 2.x for ingress, which are needed to be setup separately.
+
+## Preparation
+Download the "harbor-compose.yaml" file as well as the files located in "supplementary" directory (also archived in "supplementary.tgz") and safe the files on the swarm manager node.
+
+### Installation
+Adjust the variables located in "start-stop.sh" script according to your requirements - the mandatory vars are:
+- HARBOR_HOST
+- HARBOR_ADMIN_PASSWORD
+Make the scipt "start-stop.sh" executable:
+$ chmod u+x start-stop.sh
+
+### Startup
+Launch the stack using the script:
+$ ./start-stop.sh start
+
+Monitor the stack using following commands (depending on situation):
+$ docker stack ps harbor
+$ docker service ls -f name=harbor
---
+++ start-stop.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+export STACK="harbor"
+
+# change-me
+export HARBOR_HOST="harbor.myfqdn.domain"
+export HARBOR_ADMIN_PASSWORD="Harbor12345"
+
+# default settings (if changing the postgres pw and user, also adjust the supplementary/clair/config.yaml on the line nr. 5)
+export REGISTRY_HTTP_SECRET="harbor"
+export CORE_SECRET="harbor"
+export JOBSERVICE_SECRET="harbor"
+export POSTGRESQL_USERNAME="postgres"
+export POSTGRESQL_PASSWORD="bitnami"
+export POSTGRESQL_DATABASE="registry"
+export HARBOR_ADMIN_PASSWORD="Harbor12345"
+export HARBOR_TAG="2-debian-10"
+export PSQL_TAG="11-debian-10"
+export REDIS_TAG="6.0-debian-10"
+export NGINX_TAG="1.19-debian-10"
+export CHARTMUSEUM_TAG="0-debian-10"
+export CORE_KEY="harbor"
+
+start() { 
+  docker stack deploy -c $STACK-compose.yaml $STACK
+}
+
+stop() {
+  docker stack rm $STACK
+}
+
+case $1 in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    stop
+    sleep 10
+    start
+    ;;
+  *)
+    echo "Usage: $0 (start|stop|restart)"
+    exit 127
+    ;;
+esac
